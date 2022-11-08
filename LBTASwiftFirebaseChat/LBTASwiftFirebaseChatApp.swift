6c30ae5f830c1535461c6c2fc3a5ba72d1aff5af
Andika .R <andika15ramdani@gmail.com>
2022-11-08 17:19:29 +0700

    reinit commit

---
+++ LBTASwiftFirebaseChat/LBTASwiftFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftFirebaseChatApp.swift
+//  LBTASwiftFirebaseChat
+//
+//  Created by dev22 jumpa on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
