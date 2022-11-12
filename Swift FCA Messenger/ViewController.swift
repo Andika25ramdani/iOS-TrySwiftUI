8dacb85cac500632156f9077d7bd22c475270552
Andika .R <andika15ramdani@gmail.com>
2022-11-12 10:20:26 +0700

    - init

---
+++ Swift FCA Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift FCA Messenger
+//
+//  Created by dev22 jumpa on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
