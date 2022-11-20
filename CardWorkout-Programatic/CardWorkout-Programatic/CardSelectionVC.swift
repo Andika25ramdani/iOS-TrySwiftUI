951e2465b0748f9e7ec49d8c184dc9a8d5425d86
Andika .R <andika15ramdani@gmail.com>
2022-11-20 15:59:43 +0700

    Programatic - 1. Setup

---
+++ CardWorkout-Programatic/CardWorkout-Programatic/CardSelectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programatic
+//
+//  Created by dev22 jumpa on 20/11/22.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view?.backgroundColor = .blue
+	}
+
+
+}
+
