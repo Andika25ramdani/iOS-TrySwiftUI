47d0ed2a1f771309dd49e5cce1572ed9325734df
Andika .R <andika15ramdani@gmail.com>
2022-11-20 13:47:21 +0700

    Storyboard - 2. IBOutlet

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -1,19 +1,23 @@
-//
-//  ViewController.swift
-//  CardWorkout
-//
-//  Created by dev22 jumpa on 19/11/22.
-//
-
 import UIKit
 
 class CardSelectionVC: UIViewController {
 
+	@IBOutlet weak var cardImageView: UIImageView!
+	@IBOutlet var buttons: [UIButton]!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		for button in buttons {
+			button.layer.cornerRadius = 8
+		}
 	}
 
-
+	@IBAction func stoppedButtonTapped(_ sender: UIButton) {
+	}
+	@IBAction func restartButtonTapped(_ sender: UIButton) {
+	}
+	@IBAction func rulesButtonTapped(_ sender: UIButton) {
+	}
 }
 
